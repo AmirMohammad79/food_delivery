25e0f58a95ad968bc9c605e7901df54f3bc99387
amir mohammad <a.abdpour0881@gmail.com>
2023-07-13 23:08:03 +0430

    update food page body : section baner

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -8,25 +8,49 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85) ;
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 320,
+      color: Colors.redAccent,
       child: PageView.builder(
-        itemCount: 5,
+        controller: pageController,
+          itemCount: 5,
           itemBuilder: (context, position) {
-              return  _buildPageItem(position);
-      }),
+            return _buildPageItem(position);
+          }),
     );
   }
-  Widget _buildPageItem(int index){
-    return Container(
-      height: 220,
-      margin: const EdgeInsets.only(left: 5 , right:5 ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: index.isEven ? const Color(0xff69c5df) : const Color(0xff9294cc)
-      ),
+
+  Widget _buildPageItem(int index) {
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: const EdgeInsets.only(left: 10, right: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven
+                ? const Color(0xff69c5df)
+                : const Color(0xff9294cc),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage("assets/image/food0.png"),
+            )
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+              height: 130,
+              margin: const EdgeInsets.only(left: 30, right: 30 , bottom: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              )),
+        )
+      ],
     );
   }
 }
