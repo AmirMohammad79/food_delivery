a1f60e833ff5574e505a1ad7ce13389796bd9128
amir mohammad <a.abdpour0881@gmail.com>
2023-07-14 16:42:58 +0430

    Update  banner section : add description of images

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/icon_and_text_widget.dart';
+import 'package:food_delivery/widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({Key? key}) : super(key: key);
@@ -8,14 +12,13 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
-  PageController pageController = PageController(viewportFraction: 0.85) ;
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 320,
-      color: Colors.redAccent,
       child: PageView.builder(
-        controller: pageController,
+          controller: pageController,
           itemCount: 5,
           itemBuilder: (context, position) {
             return _buildPageItem(position);
@@ -34,21 +37,60 @@ class _FoodPageBodyState extends State<FoodPageBody> {
             color: index.isEven
                 ? const Color(0xff69c5df)
                 : const Color(0xff9294cc),
-            image: DecorationImage(
+            image: const DecorationImage(
               fit: BoxFit.cover,
               image: AssetImage("assets/image/food0.png"),
-            )
+            ),
           ),
         ),
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-              height: 130,
-              margin: const EdgeInsets.only(left: 30, right: 30 , bottom: 10),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.white,
-              )),
+            height: 120,
+            margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Chinese Side"),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                            5,
+                            (index) => Icon(
+                                  Icons.star,
+                                  color: AppColors.mainColor,
+                                )),
+                      ),
+                      const SizedBox(width: 10,),
+                      SmallText(text: "4.5" ) ,
+                      const SizedBox(width: 10,),
+                      SmallText(text: "1287"),
+                      const SizedBox(width: 10,),
+                      SmallText(text: "comment")
+                    ],
+                  ),
+                  const SizedBox(height: 20,),
+                  Row(
+                    children: [
+                    IconAndTextWidget(text: "Normal", iconColor: AppColors.iconColor1, icon: Icons.circle_sharp),
+                    IconAndTextWidget(text: "1.7km", iconColor: AppColors.mainColor, icon: Icons.location_on),
+                    IconAndTextWidget(text: "32min", iconColor: AppColors.iconColor2, icon: Icons.access_time_filled_rounded),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
         )
       ],
     );
