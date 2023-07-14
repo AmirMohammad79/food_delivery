f0898079a843b9e432e19822cf4fec1c023839a9
amir mohammad <a.abdpour0881@gmail.com>
2023-07-14 23:52:18 +0430

    add DotsIndicator in food page body

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,3 +1,4 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
@@ -13,9 +14,9 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
-  var _currPageValue = 0.0 ;
-  double _scaleFactor = 0.8 ;
-  double _height = 220 ;
+  var _currPageValue = 0.0;
+  double _scaleFactor = 0.8;
+  double _height = 220;
   @override
   void initState() {
     super.initState();
@@ -25,45 +26,61 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       });
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       children: [
-      Container(
-      height: 320,
-      child: PageView.builder(
-        controller: pageController,
-        itemCount: 5,
-        itemBuilder: (context, position) {
-          return _buildPageItem(position);
-        },
-      ),
-    ),
-
+        Container(
+          height: 320,
+          child: PageView.builder(
+            controller: pageController,
+            itemCount: 5,
+            itemBuilder: (context, position) {
+              return _buildPageItem(position);
+            },
+          ),
+        ),
+        DotsIndicator(
+          dotsCount: 5,
+          position: _currPageValue,
+          decorator: DotsDecorator(
+            activeColor: AppColors.mainColor,
+            size: const Size.square(9.0),
+            activeSize: const Size(18.0, 9.0),
+            activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)),
+          ),
+        )
       ],
     );
   }
 
   Widget _buildPageItem(int index) {
     Matrix4 matrix = Matrix4.identity();
-    if (index == _currPageValue.floor()){
-      var currScale = 1 - (_currPageValue - index) * (1- _scaleFactor);
-      var currTrans= _height * (1- currScale)/2;
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
-    }else if(index == _currPageValue.floor() + 1){
-      var currScale = _scaleFactor + (_currPageValue - index +1)*(1-_scaleFactor);
-      var currTrans= _height * (1- currScale)/2;
+    if (index == _currPageValue.floor()) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currPageValue - index + 1) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1);
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
-    }else if (index == _currPageValue.floor() -1){
-      var currScale = 1 - (_currPageValue - index) * (1- _scaleFactor);
-      var currTrans= _height * (1- currScale)/2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else if (index == _currPageValue.floor() - 1) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      var currTrans = _height * (1 - currScale) / 2;
       matrix = Matrix4.diagonal3Values(1, currScale, 1);
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
-    }else {
-      var currScale = 0.8 ;
-      var currTrans= _height * (1- currScale)/2;
-      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 1);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 0);
+    } else {
+      var currScale = 0.8;
+      var currTrans = _height * (1 - currScale) / 2;
+      matrix = Matrix4.diagonal3Values(1, currScale, 1)
+        ..setTranslationRaw(0, currTrans, 1);
     }
     return Transform(
       transform: matrix,
@@ -89,24 +106,16 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               height: 120,
               margin: const EdgeInsets.only(left: 30, right: 30, bottom: 30),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.white,
-                boxShadow: const [
-                  BoxShadow(
-                    color: Color(0xffe8e8e8),
-                    blurRadius: 5.0,
-                    offset: Offset(0,5)
-                  ),
-                  BoxShadow(
-                    color: Colors.white,
-                    offset: Offset(-5, 0)
-                  ) ,
-                  BoxShadow(
-                      color: Colors.white,
-                      offset: Offset(5, 0)
-                  )
-                ]
-              ),
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                  boxShadow: const [
+                    BoxShadow(
+                        color: Color(0xffe8e8e8),
+                        blurRadius: 5.0,
+                        offset: Offset(0, 5)),
+                    BoxShadow(color: Colors.white, offset: Offset(-5, 0)),
+                    BoxShadow(color: Colors.white, offset: Offset(5, 0))
+                  ]),
               child: Container(
                 padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
                 child: Column(
--- lib/home/main_home_page.dart
+++ lib/home/main_home_page.dart
@@ -19,6 +19,7 @@ class _MainFoodPageState extends State<MainFoodPage> {
     return  Scaffold(
       body:  Column(
         children: [
+          //show header
           Container(
             child: Container(
               margin: const EdgeInsets.only(top: 45 , bottom: 15),
@@ -54,6 +55,7 @@ class _MainFoodPageState extends State<MainFoodPage> {
               ),
             ),
           )  ,
+          //show body
           FoodPageBody()
         ],
       ),
