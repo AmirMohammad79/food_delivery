ffceb8930ef9b39259eafd363c7b50e3d1b82bfe
amir mohammad <a.abdpour0881@gmail.com>
2023-07-16 02:58:50 +0430

    change foodPageBody wrap with single scroll view

--- lib/home/main_home_page.dart
+++ lib/home/main_home_page.dart
@@ -17,47 +17,58 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:  Column(
+    return Scaffold(
+      body: Column(
         children: [
           //show header
           Container(
             child: Container(
-              margin:  EdgeInsets.only(top: Dimensions.height45 , bottom: Dimensions.height15),
-              padding:  EdgeInsets.only(left: Dimensions.width20 ,right: Dimensions.width20),
+              margin: EdgeInsets.only(
+                  top: Dimensions.height45, bottom: Dimensions.height15),
+              padding: EdgeInsets.only(
+                  left: Dimensions.width20, right: Dimensions.width20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
                     child: Column(
-                      children:  [
-                        BigText(text: "iran" , color: AppColors.mainColor),
+                      children: [
+                        BigText(text: "iran", color: AppColors.mainColor),
                         Row(
                           children: [
-                            SmallText(text: "yasuj" , color: Colors.black54),
-                            Icon(Icons.arrow_drop_down_rounded , color: Colors.black54, size: Dimensions.iconSize24,)
+                            SmallText(text: "yasuj", color: Colors.black54),
+                            Icon(
+                              Icons.arrow_drop_down_rounded,
+                              color: Colors.black54,
+                              size: Dimensions.iconSize24,
+                            )
                           ],
                         )
                       ],
                     ),
                   ),
-                Center(
-                  child: Container(
-                    height: Dimensions.height45,
-                    width: Dimensions.width45,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(Dimensions.radius15),
-                        color: AppColors.mainColor
+                  Center(
+                    child: Container(
+                      height: Dimensions.height45,
+                      width: Dimensions.width45,
+                      decoration: BoxDecoration(
+                          borderRadius:
+                              BorderRadius.circular(Dimensions.radius15),
+                          color: AppColors.mainColor),
+                      child: const Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
                     ),
-                    child: const Icon(Icons.search , color: Colors.white,),
-                  ),
-                )
+                  )
                 ],
               ),
             ),
-          )  ,
+          ),
           //show body
-          FoodPageBody()
+          Expanded(
+            child: SingleChildScrollView(child: FoodPageBody()),
+          )
         ],
       ),
     );
