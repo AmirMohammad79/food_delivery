4da9de03068c4388530108f806af0fd7a628b5f2
amir mohammad <a.abdpour0881@gmail.com>
2023-07-31 19:27:06 +0330

    add popular_product_controller.dart

---
+++ lib/controllers/popular_product_controller.dart
@@ -0,0 +1,8 @@
+import 'package:food_delivery/data/repository/popular_product_repo.dart';
+import 'package:get/get.dart';
+
+class PopularProductController extends GetxController {
+  final PopularProductRepo popularProductRepo;
+  PopularProductController({required this.popularProductRepo});
+
+}
