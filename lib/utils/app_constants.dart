b939e12ea9bf92dbd88b1c5b95ea17ef220427d6
amir mohammad <a.abdpour0881@gmail.com>
2023-08-02 20:10:28 +0330

    add new file in utils : app_constants.dart

---
+++ lib/utils/app_constants.dart
@@ -0,0 +1,11 @@
+class AppConstant{
+  static const String appName = "DBFood";
+  static const int appVersion = 1;
+
+  static const String baseUrl = "http://10.0.2.2:8041/";
+
+  static const String popularProductUri = "api/v1/products/popular";
+  static const String recommendedProductUri = "api/v1/products/recommended";
+
+  static const String token = "DBtoken";
+}
