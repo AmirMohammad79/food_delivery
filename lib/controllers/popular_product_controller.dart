7317de75b9a4462e945583099514ab6bb3570d43
amir mohammad <a.abdpour0881@gmail.com>
2023-08-30 18:56:47 +0330

    add the CircularProgressIndicator to slider in food_page_body.dart

--- lib/controllers/popular_product_controller.dart
+++ lib/controllers/popular_product_controller.dart
@@ -8,12 +8,16 @@ class PopularProductController extends GetxController {
   PopularProductController({required this.popularProductRepo});
   List<dynamic> _popularProductList = [];
   List<dynamic> get popularProductList => _popularProductList;
+
+  bool  _isLoaded = false;
+  bool get isLoaded => _isLoaded;
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
     if (response.statusCode == 200) {
       print(".............................................................................................................................get data");
       _popularProductList = [];
       _popularProductList.addAll(Product.fromJson(response.body).products);
+      _isLoaded = true;
       update();
     } else {
       print( response.statusText.toString());
--- lib/pages/home/food_page_body.dart
+++ lib/pages/home/food_page_body.dart
@@ -45,21 +45,28 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       children: [
         //slider section
         GetBuilder<PopularProductController>(builder: (popularProducts) {
-          return Container(
-            height: Dimensions.pageView,
-            child: PageView.builder(
-              controller: pageController,
-              itemCount: popularProducts.popularProductList.length,
-              itemBuilder: (context, position) {
-                return _buildPageItem(position , popularProducts.popularProductList[position]);
-              },
-            ),
-          );
+          return popularProducts.isLoaded
+              ? Container(
+                  height: Dimensions.pageView,
+                  child: PageView.builder(
+                    controller: pageController,
+                    itemCount: popularProducts.popularProductList.length,
+                    itemBuilder: (context, position) {
+                      return _buildPageItem(position,
+                          popularProducts.popularProductList[position]);
+                    },
+                  ),
+                )
+              : CircularProgressIndicator(
+                  color: AppColors.mainColor,
+                );
         }),
         // dots
         GetBuilder<PopularProductController>(builder: (popularProducts) {
           return DotsIndicator(
-            dotsCount: popularProducts.popularProductList.isEmpty?1:popularProducts.popularProductList.length,
+            dotsCount: popularProducts.popularProductList.isEmpty
+                ? 1
+                : popularProducts.popularProductList.length,
             position: _currPageValue,
             decorator: DotsDecorator(
               activeColor: AppColors.mainColor,
@@ -178,7 +185,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     );
   }
 
-  Widget _buildPageItem(int index,ProductModel popularProduct) {
+  Widget _buildPageItem(int index, ProductModel popularProduct) {
     Matrix4 matrix = Matrix4.identity();
     if (index == _currPageValue.floor()) {
       var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
@@ -217,12 +224,10 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               color: index.isEven
                   ? const Color(0xff69c5df)
                   : const Color(0xff9294cc),
-              image:  DecorationImage(
-                fit: BoxFit.cover,
-                image: NetworkImage(
-                  AppConstant.baseUrl + "/uploads/"+ popularProduct.img!
-                )
-              ),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      AppConstant.baseUrl + "/uploads/" + popularProduct.img!)),
             ),
           ),
           Align(
